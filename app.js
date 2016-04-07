862ae022404b79b3dd55713409182dab2302794a
Niklan <niklanrus@gmail.com>
2016-04-07 23:52:11 +0500

    Added TOKEN file contain token for bot.

--- app.js
+++ app.js
@@ -2,23 +2,40 @@
  * @file
  * Main code base.
  */
-var TelegramBot = require('node-telegram-bot-api');
-var Telegram = new TelegramBot('', {polling: true});
-var DrupalerBot = require('./src/drupalerbot.js');
+var fs = require('fs');
+var $TOKEN;
+fs.readFile('TOKEN', 'utf8', function (err, data) {
+  if (err) {
+    process.exit();
+  }
+  $TOKEN = String(data);
+  bootstrap($TOKEN);
+});
 
 /**
- * Project information.
- *
- * @command
- *  /project [project_name]
- *  /p [project_name]
+ * Boostrap bot after geting token.
+ * @param $TOKEN
  */
-Telegram.onText(/\/(project|p) (.+)/, function (msg, match) {
-  DrupalerBot.getProjectInfo(match[2], function (message) {
-    console.log(msg);
-    Telegram.sendMessage(msg.from.id, message, {
-      parse_mode: 'Markdown',
-      disable_web_page_preview: true
+function bootstrap($TOKEN) {
+  console.log($TOKEN);
+  var TelegramBot = require('node-telegram-bot-api');
+  var Telegram = new TelegramBot($TOKEN, {polling: true});
+  var DrupalerBot = require('./src/drupalerbot.js');
+
+  /**
+   * Project information.
+   *
+   * @command
+   *  /project [project_name]
+   *  /p [project_name]
+   */
+  Telegram.onText(/\/(project|p) (.+)/, function (msg, match) {
+    DrupalerBot.getProjectInfo(match[2], function (message) {
+      Telegram.sendMessage(msg.chat.id, message, {
+        parse_mode: 'Markdown',
+        disable_web_page_preview: true
+      });
     });
   });
-});
+}
+
